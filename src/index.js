b4418115c7352e336601cd2390f3b8a2103f8ffd
Abhisek0770 <avksmlavk@gmail.com>
2024-05-13 21:28:08 +0530

    rating functionality added

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <StarRating maxRating={5}></StarRating>
-    <StarRating maxRating={10}></StarRating>
-    <StarRating />
   </React.StrictMode>
 );
 
